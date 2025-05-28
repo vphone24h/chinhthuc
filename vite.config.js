f381ac5c53b6955ecb3bee0f3bc21ae3c474aaa1
Lê Vũ <vphone24h@iMac-cua-Le.local>
2025-05-28 11:48:06 +0700

    ♻️ Đổi tên NhapHang.jsx để ép Vercel rebuild

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // Căn cứ deploy trên root domain, đổi nếu deploy trong subfolder
   plugins: [react()],
   server: {
-    port: 5174,  // port dev server bạn dùng, có thể đổi nếu muốn
-    open: true,  // tự mở trình duyệt khi chạy dev
+    port: 5174,
+    open: true,
   },
   build: {
-    outDir: 'dist',  // thư mục build ra (mặc định là dist)
+    outDir: 'dist',
   },
   resolve: {
     alias: {
-      '@': '/src',  // viết tắt đường dẫn cho src
+      '@': '/src',
     },
   },
 });
